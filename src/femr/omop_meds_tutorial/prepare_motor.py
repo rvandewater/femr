abef65b8f1d3e9bd9c28ca3e186d5421e98e477f
Robin van de Water <r.p.vandewater@gmail.com>
2025-08-02 12:16:10 +0200

    add option for splits

--- src/femr/omop_meds_tutorial/prepare_motor.py
+++ src/femr/omop_meds_tutorial/prepare_motor.py
@@ -40,19 +40,26 @@ def main(args):
         else:
             with open(ontology_path, 'rb') as f:
                 ontology = pickle.load(f)
+        if not args.pre_split:
+            subject_splits = pd.read_parquet(subject_splits_path)
+            subject_splits = subject_splits[subject_splits.subject_id.isin(subject_ids)]
+            train_tuning_split = subject_splits[~subject_splits.split.isin(["held_out"])].subject_id.tolist()
+            test_split = subject_splits[subject_splits.split.isin(["held_out"])].subject_id.tolist()
+            main_split = femr.splits.SubjectSplit(train_tuning_split, test_split)
+            main_split.save_to_csv(str(pretraining_data_path / 'main_split.csv'))
 
-        subject_splits = pd.read_parquet(subject_splits_path)
-        subject_splits = subject_splits[subject_splits.subject_id.isin(subject_ids)]
-        train_tuning_split = subject_splits[~subject_splits.split.isin(["held_out"])].subject_id.tolist()
-        test_split = subject_splits[subject_splits.split.isin(["held_out"])].subject_id.tolist()
-        main_split = femr.splits.SubjectSplit(train_tuning_split, test_split)
-        main_split.save_to_csv(str(pretraining_data_path / 'main_split.csv'))
-
-        train_split = femr.splits.generate_hash_split(main_split.train_subject_ids, 17, frac_test=0.05)
-
-        main_database = database.filter(main_split.train_subject_ids)
-        train_database = main_database.filter(train_split.train_subject_ids)
-        val_database = main_database.filter(train_split.test_subject_ids)
+            train_split = femr.splits.generate_hash_split(main_split.train_subject_ids, 17, frac_test=0.05)
+            train_val_ids = main_split.train_subject_ids
+            train_ids = train_split.train_subject_ids
+            val_ids = train_split.test_subject_ids
+        else:
+            subject_splits = pl.read_parquet(subject_splits_path)
+            train_val_ids = subject_splits.filter(pl.col("split") != "held_out").select("subject_id").to_series().to_list()
+            train_ids = subject_splits.filter(pl.col("split") == "train").select("subject_id").to_series().to_list()
+            val_ids = subject_splits.filter(pl.col("split") == "tuning").select("subject_id").to_series().to_list()
+        main_database = database.filter(train_val_ids)
+        train_database = main_database.filter(train_ids)
+        val_database = main_database.filter(val_ids)
 
         tokenizer_path = pretraining_data_path / 'tokenizer'
         if not tokenizer_path.exists():
@@ -165,6 +172,8 @@ def create_omop_meds_tutorial_argparser():
         # this is decided based on the 99% percentile of the number of tokens
         default=8192,
     )
+    parser.add_argument("--pre_split", action="store_true", help="Whether the splits have been "
+                                                                "generated already in train, tuning, and held_out sets")
     return parser
 
 
