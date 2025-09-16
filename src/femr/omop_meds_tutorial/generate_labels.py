c4bc7a2caac2d3d240c3fe967084ac640989bd58
Robin van de Water <r.p.vandewater@gmail.com>
2025-09-16 11:51:43 +0200

    within 30 days

--- src/femr/omop_meds_tutorial/generate_labels.py
+++ src/femr/omop_meds_tutorial/generate_labels.py
@@ -97,12 +97,14 @@ class OmopInpatientMortalityLabeler(femr.labelers.Labeler):
             if prediction_time >= death_time:
                 print(f"Warning: prediction time {prediction_time} is after death time {death_time} for subject {subject.subject_id}")
                 continue
-            if death_time > admission_end:
-                print(f"Warning: death time {death_time} is after prediction time {prediction_time} for subject {subject.subject_id}")
+            # if death_time > admission_end:
+            #     print(f"Warning: death time {death_time} is after prediction time {prediction_time} for subject {subject.subject_id}")
                 # continue
             is_death = death_time < prediction_time #< admission_end
             if is_death:
                 print(f"Labeling subject {subject.subject_id} as death at prediction time {prediction_time} at {death_time_admission_start} with death time {death_time} and admission end {admission_end}")
+                if death_time > admission_end:
+                    print(f"warning for subject {subject.subject_id} death is : {death_time-admission_end} after discharge")
             labels.append(
                 meds.Label(subject_id=subject.subject_id, prediction_time=prediction_time, boolean_value=is_death))
 
