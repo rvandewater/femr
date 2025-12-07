f6217e6feac9ad991b78ae348e59f3dbd9cb24bf
Robin van de Water <r.p.vandewater@gmail.com>
2025-12-07 12:00:50 +0100

    chnage path

--- src/femr/omop_meds_tutorial/finetune_motor.py
+++ src/femr/omop_meds_tutorial/finetune_motor.py
@@ -69,8 +69,8 @@ def main():
             if test_result_file.exists():
                 print(f"The result already existed for {label_name} at {test_result_file}, it will be skipped!")
                 continue
-            labels = pd.read_parquet(pretraining_data / "labels" / (label_name + '.parquet'))
-
+            # labels = pd.read_parquet(pretraining_data / "labels" / (label_name + '.parquet'))
+            labels = pd.read_parquet(f"{label_path}/**/*.parquet")
             motor_features_name = get_motor_features_name(label_name, args.observation_window)
             with open(pretraining_data / 'features' / f"{motor_features_name}.pkl", 'rb') as f:
                 features = pickle.load(f)
